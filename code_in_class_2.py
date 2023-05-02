523583180cfde978a8793deb29a62d733a8ad276
MelanyaManukyan <m.manukyan1987@gmail.com>
2023-05-02 20:00:25 +0400

    clean code

--- code_in_class_2.py
+++ code_in_class_2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pytesseract
 import numpy as np
 import cv2
-
+#երկու ֆունկցիան կպցրած է իրար
 #  path pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'D:\ProgramData\tesseract.exe'
 
@@ -11,6 +11,7 @@ pytesseract.pytesseract.tesseract_cmd = r'D:\ProgramData\tesseract.exe'
 class ImageProcesing:
     def __init__(self, first_image_path):
         self.first_image = first_image_path
+        # self.rotated_image=rotated_image
 
     def rotated_img(self):
         first_image_path = "k.png"
@@ -23,13 +24,13 @@ class ImageProcesing:
 # Using cv2.ROTATE_90_COUNTERCLOCKWISE
 # rotate by 270 degrees clockwise
         rotated_image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
+        print(rotated_image.shape)
 # Displaying the image
         cv2.imshow(window_name, rotated_image)
         cv2.waitKey(0)
 
-    def marked_img(self, rotated_image):
-        self.rotated_image = rotated_image
+    # def marked_img(self,rotated_image):
+        # self.rotated_image = rotated_image
         gray = cv2.cvtColor(rotated_image, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
         kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
@@ -45,11 +46,36 @@ class ImageProcesing:
                 continue
         cv2.rectangle(rotated_image, (x, y), (x+w, y+h), (0, 0, 255), 4)
     # write original image with added contours to disk
-        return cv2.imwrite('contoured.jpg', rotated_image)
+        print(rotated_image)
+        # return cv2.imwrite('contoured.jpg', rotated_image)
+        cv2.imwrite('contoured.jpg', rotated_image)
+    def show_image(self):
+        #show imnage
+        plt.imshow(rotated_image)
+        plt.show()
 
+        return show_image(cv2.imread(rotated_image))
+
+
+
+with open("text_by_photo.txt", "w") as f:
+
+    text = pytesseract.image_to_string("k.png")
+
+    f.write(text)
+
+    # read each line
+with open("text_by_photo.txt", "r") as f:
+    content = f.readlines()
+for line in range(len(content)):
+    # print(len(content))
+    count_line = int(input("Enter line_number --> "))
+    if count_line > len(content):
+        print(f"Please enter num in range(1: {len(content)})")
+    print(linecache.getline(("text_by_photo.txt"), count_line))
 
 rot_i = ImageProcesing("path")
-mark_i = ImageProcesing("path")
-
+# mark_i = ImageProcesing("path")
+rot_i.rotated_img()
 mark_i.marked_img(rot_i.rotated_img())
 
